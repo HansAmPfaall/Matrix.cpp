502f2e637b281c80b79089a931767a3a86d0ae78
HansAmPfaall <EdgarJustus94@gmail.com>
2019-02-22 20:08:04 +0100

    TEST

--- Matrix.h
+++ Matrix.h
@@ -3,26 +3,25 @@
 #include <vector>
 #include "./Matrix.cpp"
 
-class Matrix
-{
-  public:
-    // Construct a new instance.
-    Matrix(size_t rows, size_t cols);
+class Matrix {
+ public:
+  // Construct a new instance.
+  Matrix(size_t rows, size_t cols);
 
-    // Destructor.
-    ~Matrix();
+  // Destructor.
+  ~Matrix();
 
-    void print();
+  void print();
 
-    Matrix mult(Matrix m);
+  Matrix mult(Matrix m);
 
-    void setValue(int row, int col, double value);
+  void setValue(int row, int col, double value);
 
-    Matrix transpose();
+  Matrix transpose();
 
-  private:
-    // Size of the storage field.
-    size_t _rowNum;
-    size_t _colNum;
-    std::vector<std::vector<double> > _storage;
+ private:
+  // Size of the storage field.
+  size_t _rowNum;
+  size_t _colNum;
+  std::vector<std::vector<double> > _storage;
 };
